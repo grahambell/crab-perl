8abcd93d9897765406f732ec93731eb04ca56112
Graham Bell <g.bell@eaobservatory.org>
2016-01-26 09:48:25 -1000

    README: don't list Perl as a requirement

--- README.sh
+++ README.sh
@@ -18,7 +18,7 @@ Requirements
 
 END
 
-perl -e 'local $\ = "\n"; $INC{"Module/Build.pm"} = 1; do "Build.PL"; print "    ", $_ foreach sort keys %require;'
+perl -e 'local $\ = "\n"; $INC{"Module/Build.pm"} = 1; do "Build.PL"; print "    ", $_ foreach grep {$_ ne "perl"} sort keys %require;'
 
 cat <<END
 
